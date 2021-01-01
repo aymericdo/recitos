5b40d13623d6888823f8aa4190797eb76314870e
Aymeric Dominique <aymeric.dominique@gmail.com>
2021-01-01 23:43:03 +0100

    add pause btn + some colors

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,15 +6,17 @@
     <div class="col" v-if="sliderColor">
       <round-slider
         v-model="tempo"
+        :change="sliderChanged"
         start-angle="315"
         end-angle="+270"
         line-cap="round"
         max="45"
         min="1"
-        v-bind:pathColor="sliderColor[0]"
-        v-bind:rangeColor="sliderColor[1]"
-        v-bind:tooltipColor="sliderColor[1]"
+        :pathColor="sliderColor[0]"
+        :rangeColor="sliderColor[1]"
+        :tooltipColor="sliderColor[1]"
       />
+      <button v-on:click="pause()">{{ onPause ? 'Play' : 'Pause' }}</button>
     </div>
   </div>
 </template>
@@ -33,6 +35,9 @@ export default {
       ['#38ada9', '#fa983a'],
       ['#8e44ad', '#f1c40f'],
       ['#c8d6e5', '#2e86de'],
+      ['#6D214F', '#FD7272'],
+      ['#218c74', '#33d9b2'],
+      ['#9980FA', '#009432'],
     ];
     const rand = Math.floor(Math.random() * Math.floor(sliderColors.length));
     this.sliderColor = sliderColors[rand];
@@ -48,6 +53,10 @@ export default {
     }
   },
   methods: {
+    sliderChanged() {
+      clearInterval(this.intervalId);
+      this.valueChanged({ target: { value: this.value } });
+    },
     valueChanged(event) {
       const value = event.target.value;
 
@@ -64,14 +73,17 @@ export default {
 
         // Store the id of the interval so we can clear it later
         this.intervalId = setInterval(() => {
-          this.value = this.value.slice(0, -1);
-          startInterval(1000);
+          if (!this.onPause) {
+            this.value = this.value.slice(0, -1);
+            startInterval(1000);
+          }
         }, _interval);
       }
 
-      if (!this.onPause) {
-        startInterval(this.tempo * 1000);
-      }
+      startInterval(this.tempo * 1000);
+    },
+    pause() {
+      this.onPause = !this.onPause;
     }
   }
 }
